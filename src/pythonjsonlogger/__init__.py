32f763fd2c243a1e8c9dab756c7bb83810a75dcc
Nicholas Hairs <info@nicholashairs.com>
2024-05-03 18:19:58 +1000

    simplify ORJSON_AVAILABLE check

--- src/pythonjsonlogger/__init__.py
+++ src/pythonjsonlogger/__init__.py
@@ -3,7 +3,6 @@
 ## Future
 
 ## Standard Library
-import sys
 import warnings
 
 ## Installed
@@ -13,17 +12,12 @@ import pythonjsonlogger.json
 
 ### CONSTANTS
 ### ============================================================================
-if sys.implementation.name == "pypy":
-    # Per https://github.com/ijl/orjson (last checked 2024-04-28)
-    # > orjson does not and will not support PyPy
-    ORJSON_AVAILABLE = False
-else:
-    try:
-        import orjson
+try:
+    import orjson
 
-        ORJSON_AVAILABLE = True
-    except ImportError:
-        ORJSON_AVAILABLE = False
+    ORJSON_AVAILABLE = True
+except ImportError:
+    ORJSON_AVAILABLE = False
 
 
 try:
