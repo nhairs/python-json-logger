cb81106b82015b2bada63e0242fa30711e254ecf
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2025-07-17 14:15:26 +0200

    [docs] Update version added for exc_info_as_array, stack_info_as_array

--- src/pythonjsonlogger/core.py
+++ src/pythonjsonlogger/core.py
@@ -135,7 +135,7 @@ class BaseJsonFormatter(logging.Formatter):
 
     *Changed in 3.2*: `defaults` argument is no longer ignored.
 
-    *Added in UNRELEASED*: `exc_info_as_array` and `stack_info_as_array` options are added.
+    *Added in 3.3*: `exc_info_as_array` and `stack_info_as_array` options are added.
     """
 
     _style: Union[logging.PercentStyle, str]  # type: ignore[assignment]
